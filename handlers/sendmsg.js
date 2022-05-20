e9f446bc7497cddfca5f743ecb5abba0f0fb9ac2
VasudevanVaisnav <vaisnav21032001@gmail.com>
2022-05-20 20:06:45 +0530

    sendMsg Changed

--- handlers/sendmsg.js
+++ handlers/sendmsg.js
@@ -2,31 +2,26 @@ const fcm = require("fcm-node")
 const user = require("../models/user.js");
 const sender = new fcm('AAAABvcFeQk:APA91bG09Dp52XI0HV9ZZKplgdzyeiqhrURqoXVXMFp-DbfcmqJAQ_-C_iO10MCBIFuT0nYwypYYC8KfEmcid5RF1oS1S7jdT710Erp9hU9NZL0wcda-TjwKBMSxMHvO5yU1WkTgn-_g');
 function sendmsg(req,res,err){
-   
-    var registrationTokens = [];
-    const message = new gcm.Message({ 
-        priority:'high',
-        contentAvailable:true,
-        delayWhileIdle:true,
-        timeToLive:86400,
-    });
+
+    var messagef = { 
+        data: {  
+        }
+    };
     console.log("sendmsg");
     console.log(req.body);
     const msg = req.body.message;
     const key = req.body.key;
     const rec = req.body.to;
     const sen = req.body.from;
-    message.addData('fromNum',sen);
-    message.addData('fromName',sen);
-    message.addData('key',key);
-    message.addData('message',msg);
+    messagef.data.fromNum = sen;
+    messagef.data.fromName = sen;
+    messagef.data.key = key;
+    messagef.data.message = msg;
     user.findOne({"mobile":rec},(ferr,fres)=>{
         if (!ferr && fres){
-            
-            registrationTokens = [];
-            registrationTokens.push(fres.address)
-            console.log(registrationTokens);
-            sender.send(message, { registrationTokens: registrationTokens }, 10, function (err, response) {
+            messagef.to = fres.address;
+            console.log(messagef);
+            sender.send(messagef, function (err, response) {
                 if(err){
                     console.log("failure");
                     console.log(err);
